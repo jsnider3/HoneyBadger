abda19161083adcbf89e0c4823fcc19c07c0dcc2
Joshua Snider <jsnider3@masonlive.gmu.edu>
2015-05-11 15:40:11 -0400

    Refactoring

--- tester.py
+++ tester.py
@@ -2,8 +2,8 @@ import os
 import subprocess
 import unittest
 
-INDIR = "tests/input/"
-OUTDIR = "tests/output/"
+IN = "tests/input/"
+OUT = "tests/output/"
 
 class Tests(unittest.TestCase):
 
@@ -17,61 +17,57 @@ class Tests(unittest.TestCase):
     return lines
 
   def dotest(self, fname):
-    assert(self.run_HB(INDIR+fname+".hnb") == self.cat(OUTDIR+fname+".out"))
+    assert(self.run_HB(IN + fname + ".hnb") == self.cat(OUT + fname + ".out"))
+
+  def test_args(self):
+    self.dotest("argOrder")
+    self.dotest("multiarg")
 
   def test_arith(self):
     self.dotest("arith")
     self.dotest("arith2")
     self.dotest("div")
 
+  def test_arr(self):
+    self.dotest("index")
+    self.dotest("set")
+    self.dotest("len")
+    self.dotest("concat")
+
+  def test_casts(self):
+    self.dotest("boolcasts")
+
   def test_func(self):
     self.dotest("func1")
     self.dotest("func2")
-
-  def test_scope(self):
-    self.dotest("scope")
-    self.dotest("scope2")
-
-  def test_fac(self):
     self.dotest("fac")
-
-  def test_16(self):
-    self.dotest("test16")
-
-  def test_helloworld(self):
-    self.dotest("helloworld")
-
-  def test_while(self):
-    self.dotest("while")
 
   def test_if(self):
     self.dotest("ifor")
     self.dotest("if2")
     self.dotest("if3")
 
-  def test_casts(self):
-    self.dotest("boolcasts")
-
-  def test_seq(self):
-    self.dotest("seq")
-
-  def test_arr(self):
-    self.dotest("index")
-    self.dotest("set")
-    self.dotest("len")
-
-  def test_concat(self):
-    self.dotest("concat")
-
-  def test_9(self):
-    self.dotest("test9")
+  def test_print(self):
+    self.dotest("helloworld")
 
   def test_qsort(self):
     self.dotest("qsort")
 
-  def test_args(self):
-    self.dotest("argOrder")
-    self.dotest("multiarg")
+  def test_scope(self):
+    self.dotest("scope")
+    self.dotest("scope2")
+
+  def test_seq(self):
+    self.dotest("seq")
+
+  def test_while(self):
+    self.dotest("while")
+
+  def test_9(self):
+    self.dotest("test9")
+
+  def test_16(self):
+    self.dotest("test16")
 
 if __name__ == '__main__':
   unittest.main()
